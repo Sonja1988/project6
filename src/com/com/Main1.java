56c8da3ea67c580e13cd86b8fd69513e211a532b
Sonja1988 <sonja.miloshevska@gmail.com>
2022-06-21 19:02:41 +0200

    update main class

--- src/com/com/Main1.java
+++ src/com/com/Main1.java
@@ -4,5 +4,7 @@ public class Main1 {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
+        System.out.println("commit1");
+        System.out.println("commit2");
     }
 }
